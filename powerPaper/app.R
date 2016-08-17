c282491f26443bbdb612308e222a7795d52e5384
Jorge <jahumada@conservation.org>
2016-08-17 12:44:22 -0400

    First working version

---
+++ powerPaper/app.R
@@ -0,0 +1,74 @@
+# R code for power app
+
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(reshape2)
+library(png)
+library(grid)
+library(unmarked)
+library(parallel)
+library(foreach)
+library(doParallel)
+
+
+ui <- fluidPage(theme = "style.css",
+                tags$head(
+                        tags$style(HTML("body{ overflow-x: hidden !important; padding: 0px 10px !important;}"))),
+                
+                titlePanel(tags$strong(tags$h1("How much change can you detect in your camera trapping project?"))),
+                
+                tags$hr(),
+        
+                fluidRow(wellPanel(tags$p("Success of a monitoring project based on species occupancy from camera traps depends largely on the ability of the monitoring system to detect a given level of change. With this app you can interactively explore how various design features of a camera trapping project affect the ability to detect a given level of change in occupancy under various conditions: number of points sampled per year, number of days the camera traps are running, the initial occupancy of the species, and the detection probability."))),
+        
+                fluidRow(
+                        #column(12, htmlOutput("result"), offset = 4),
+                        column(5,
+                                selectInput(inputId = "change", label = "How much annual change you want to detect?", choices = c("1%","5%","10%","15%"),selected = "5%"),
+                                sliderInput(inputId = "points", label = "How many camera trap points are you deploying?", value = 10, min = 10, max = 120, step = 10),
+                                sliderInput(inputId = "days", label = "How many days are you leaving the camera traps in the field?", value = 30, min = 15, max = 60, step = 15),
+                                sliderInput(inputId = "psi1", label = "What is the initial occupancy of the species?", value = 0.5, min = 0.1, max = 0.9, step = 0.2),
+                                selectInput(inputId = "p", label = "WHat is the detection probability of the species?", choices = c(0.1, 0.2, 0.5), selected = 0.5),
+                       
+                                tags$hr()),
+                
+                        column(7,plotOutput("plot")))
+)
+
+
+powerData <- read.csv("det.year_2016-08-06_FINAL.csv", h=T)
+
+#data generator function
+#Simulate trend function
+
+
+server <- function(input, output, session){
+        #Get the year
+        myYear <- reactive({
+                row <- filter(powerData, pts == input$points, days == input$days, psi1 == input$psi1, p == input$p, percent == input$change)
+                mydata <- row$year1st
+                if(is.na(mydata)){
+                        mytext <- "more than 10"
+                        myvalue <- 11
+                }
+                else {
+                        mytext <- mydata
+                        myvalue <- mydata
+                }
+                list(mytext = mytext, myvalue = myvalue)
+        })
+        
+        
+        
+
+        
+        
+        output$plot <- renderPlot({
+                ggplot(filter(powerData, percent == input$change), aes(x=pts, y = year1st, color = psi1, shape = as.character(p))) + geom_jitter() + geom_vline(xintercept = input$points) + geom_hline(yintercept = myYear()$myvalue) +  xlab("Number of camera trap points") + ylab("Number of years to detect change") + scale_shape_discrete(guide = guide_legend(title = "Det. probability")) + scale_color_gradient(guide = guide_legend(title = "Initial\noccupancy")) + ggtitle(paste(myYear()$mytext," years of data to detect an annual change of ",input$change))
+        })
+        
+        
+}
+
+shinyApp(ui = ui, server = server)
