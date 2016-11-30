bef23d4d603081c4efb49913501208c6815ee5c0
Jorge <jahumada@conservation.org>
2016-11-30 18:21:50 -0500

    New version of PowerApp

--- ShinyApps/powerPaper/app.R
+++ ShinyApps/powerPaper/app.R
@@ -1,4 +1,5 @@
 # R code for power app
+# Jorge A. Ahumada -- Conservation International 
 
 library(shiny)
 library(ggplot2)
@@ -6,35 +7,65 @@ library(dplyr)
 library(reshape2)
 library(png)
 library(grid)
+library(tidyverse)
 
 
 ui <- fluidPage(theme = "style.css",
-                tags$head(
-                        tags$style(HTML("body{ overflow-x: hidden !important; padding: 0px 10px !important;}"))),
+                tags$head(tags$style(HTML("body{ overflow-x: hidden !important; padding: 0px 10px !important;}"))),
                 
-                titlePanel(tags$strong(tags$h1("PowerSensor!"),tags$h2("How much change can you detect in your occupancy-based monitoring project?"))),
+        fluidRow(
                 
-                #tags$hr(),
-        
-                fluidRow(wellPanel(tags$p("Success of a monitoring project based on species occupancy  depends largely on the ability of the monitoring system to detect a given level of change. With this app you can interactively explore how various design features of a monitoring project affect the ability to detect a given level of change in occupancy under various conditions: number of points sampled per year, number of days the sensors are running, the initial occupancy of the species, and the detection probability. The horizontal line in the graph shows the number of years it takes to detect the selected amount of change in occupancy."))),
+                column(10, align = "left",
+                       titlePanel(tags$strong(tags$h1("PowerSensor!"),tags$h2("How much change can you detect in your occupancy-based monitoring project?")))
+                        ),
+                column(2,
+                        tags$img(height = 28*3, width = 42*3, src = "SmallLogo.png")
+                        )
+                ),
         
                 fluidRow(
-                        #column(12, htmlOutput("result"), offset = 4),
-                        column(5,
-                                selectInput(inputId = "change", label = "How much annual change you want to detect?", choices = c("1%","5%","10%","15%"),selected = "5%"),
-                               selectInput(inputId = "alpha", label = "What confidence interval do you want to use to detect change?", choices = c("80%","90%","95%"),selected = "80%"),
-                                sliderInput(inputId = "points", label = "How many sampling points are you deploying?", value = 10, min = 0, max = 120, step = 10),
-                                selectInput(inputId = "days", label = "How many days are you leaving your sensors in the field?", choices = c(1,15,30,45,60), selected = 15),
-                                sliderInput(inputId = "psi1", label = "What is the initial occupancy of the species?", value = 0.5, min = 0.1, max = 1, step = 0.1),
-                                sliderInput(inputId = "p", label = "What is the detection probability of the species?", value = 0.5, min = 0.1, max = 0.5, step = 0.1),
-                       
-                                tags$hr()),
+                        wellPanel(tags$p("Success of a monitoring project based on species occupancy  depends largely on the ability of the monitoring system to detect a given level of change. With this app you can interactively explore how various design features of a monitoring project affect the ability to detect a given level of change in occupancy under various conditions: number of points sampled per year, number of days the sensors are running, the initial occupancy of the species, and the detection probability. The horizontal line in the graph shows the number of years it takes to detect the selected amount of change in occupancy."))
+                ),
+        
+        fluidRow(
+                column(4, align = "center", 
+                        tags$br(),
+                        tags$h4("Expected change and power", style = "color:blue"),
+                        tags$hr(),
+                       wellPanel(
+                        selectInput(inputId = "change", label = "How much annual change you want to detect?", choices = c("1%","5%","10%","15%"),selected = "5%"),
+                        selectInput(inputId = "alpha", label = "What confidence interval do you want to use to detect change?", choices = c("80%","90%","95%"),selected = "80%")
+                       )
+                       ),
                 
-                        column(7,plotOutput("plot")))
-)
+                column(4,align = "center",
+                       tags$br(),
+                       tags$h4("Field design parameters", style = "color:blue"),
+                       tags$hr(),
+                       wellPanel(
+                        sliderInput(inputId = "points", label = "How many sampling points are you deploying?", value = 30, min = 10, max = 120, step = 10),
+                        sliderInput(inputId = "days", label = "How many days are you leaving your sensors in the field?", value = 30, min = 10, max = 60, step = 10)
+                       )
+                       ),
+                
+                column(4, align = "center",
+                       tags$br(),
+                       tags$h4("Species parameters", style = "color:blue"),
+                       tags$hr(),
+                       wellPanel(
+                        sliderInput(inputId = "psi1", label = "What is the initial occupancy of the species?", value = 0.5, min = 0.1, max = 1, step = 0.1),
+                        sliderInput(inputId = "p", label = "What is the detection probability of the species?", value = 0.5, min = 0.1, max = 0.5, step = 0.1)
+                       )
+                       )
+        
+                ),
+        
+        fluidRow(
+                column(12,align = "center", tags$hr(), h4(tags$strong(textOutput("message"))), plotOutput("plot"), tags$hr()))
+        )
 
 
-powerData <- read.csv("det.year_2016-09-04_1CTremoved.csv", h=T)
+powerData <- readRDS("data")
 powerData$percent <- NA
 powerData$percent[which(powerData$phi == 0.99)] <- "1%"
 powerData$percent[which(powerData$phi == 0.95)] <- "5%"
@@ -46,33 +77,50 @@ powerData$percent[which(powerData$phi == 0.85)] <- "15%"
 
 
 server <- function(input, output, session){
-        #Get the year
-        myYear <- reactive({
+        #Get the data
+        myData <- reactive({
                 row <- filter(powerData, pts == input$points, days == input$days, psi1 == input$psi1, p == input$p, percent == input$change)
                 
+                myPts <- row$pts
+                
                 if(input$alpha == "80%") {
-                        mydata <- row$z.first80
+                        myyear <- row$z.first80
                         col <- "z.first80"
+                        lowerCI <- row[9:18]
+                        higherCI <- row[19:28]
+                        mean <- row[29:38]
+                        median <- row[39:48]
                 }
                 else if(input$alpha == "90%") {
-                        mydata <- row$z.first90
+                        myyear <- row$z.first90
                         col <- "z.first90"
-                }
-                else {
-                        mydata <- row$z.first95
-                        col <- "z.first95"
+                        lowerCI <- row[49:58]
+                        higherCI <- row[59:68]
+                        mean <- row[69:78]
+                        median <- row[79:88]
                 }
                 
-                if(!mydata){
+                else {
+                        myyear <- row$z.first95
+                        col <- "z.first95"
+                        lowerCI <- row[89:98]
+                        higherCI <- row[99:108]
+                        mean <- row[109:118]
+                        median <- row[119:128]
+                }
+                
+                
+                if(!myyear){
                         mytext <- "more than 10"
                         myvalue <- 11
                 }
                 else {
-                        mytext <- mydata
-                        myvalue <- mydata
+                        mytext <- myyear + 1
+                        myvalue <- myyear + 1
                 }
                 
-                list(mytext = mytext, myvalue = myvalue, col =col)
+                list(mytext = mytext, myvalue = myvalue, col =col, myPts = myPts, lowerCI = lowerCI, higherCI = higherCI,
+                     mean = mean, median = median)
         })
         
         
@@ -81,11 +129,14 @@ server <- function(input, output, session){
         
         
         output$plot <- renderPlot({
-                mypowerData <- filter(powerData, percent == input$change)
-                ggplot(mypowerData, aes(x=pts, y = get(myYear()$col), color = psi1, shape = as.character(p))) + geom_jitter(size=2)  + geom_hline(yintercept = myYear()$myvalue, linetype = 2, size = 1.5) +  xlab("Number of camera trap points") + ylab("Number of years to detect change") + scale_shape_discrete(guide = guide_legend(title = "Det. probability")) + scale_color_gradient(guide = guide_legend(title = "Initial\noccupancy")) + ggtitle(paste(myYear()$mytext," years of data to detect\n an annual change of ",input$change))+theme(plot.title = element_text(size=rel(2), face="bold"), axis.title.y = element_text(size=rel(2)), axis.title.x = element_text(size=rel(2)),axis.text = element_text(size=rel(1.5)), legend.text = element_text(size=rel(1.2)), legend.title = element_text(size=rel(1.3)) )
+               
+                graphData <- data.frame(year = 1:10, mean = t(myData()$mean), median = t(myData()$median), lo = t(myData()$lowerCI), hi = t(myData()$higherCI))
+        ggplot(graphData, aes(x=year)) + geom_line(aes(y=mean),size=2) + geom_ribbon(aes(ymin=lo,ymax=hi),alpha=0.2) + xlab("Time period") + ylab("Occupancy") + ylim(0,1) +  geom_vline(xintercept = myData()$myvalue) + theme(plot.title = element_text(size=rel(1.5), face="bold"), axis.text = element_text(size = rel(1.5), face = "bold"), axis.title = element_text(size=rel(1.5), face="bold")) + scale_x_continuous(breaks=seq(1, 10, 1))
         })
         
         
+        output$message <- renderText({paste(myData()$mytext," years of data to detect an annual change of ",input$change)})
+
 }
 
 shinyApp(ui = ui, server = server)
